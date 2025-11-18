c2c124e5086a7031a6d42749f0649dd1ace8ffd1
AhmdBlack0 <AhmdBlack.0@gmail.com>
2025-11-18 21:50:59 +0200

    login

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,15 +1,49 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
+// jwt
+// token => { header({alg, HS265}, type(jwt)). payload .alg(secret key)}
+// role = admin
+// payload => {id, role}
 export const register = async (req, res) => {
   try {
     const { username, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new User({
       username,
       email,
-      password,
+      password: hashedPassword,
     });
     await newUser.save();
-    res.status(201).json({ data: newUser });
+    // const userResponse = newUser.toObject();
+    // delete userResponse.password, userResponse.createdAt;
+    res.status(201).json({
+      data: {
+        username: newUser.username,
+        email: newUser.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.massage });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    // true / false
+    const correctPassword = await bcrypt.compare(password, user.password);
+    if (!correctPassword) {
+      return res.status(400).json({ data: "password is wrong" });
+    }
+    const token = jwt.sign(
+      { userId: user._id },
+      "7ecd6956bd56a3e7ddb0dc3f8a1dcb77dca0285465a859c523d63be6c4e16665",
+      { expiresIn: "3h" }
+    );
+    return res.status(200).json({ data: user });
   } catch (error) {
     res.status(500).json({ error: error.massage });
   }
--- models/user.model.js
+++ models/user.model.js
@@ -9,10 +9,14 @@ const userSchema = mongoose.Schema(
     email: {
       type: String,
       required: true,
+      unique: true,
     },
     password: {
       type: String,
       required: true,
+    },
+    token: {
+      type: String,
     },
   },
   {
