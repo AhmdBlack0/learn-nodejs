6c36d3c227b037031e16eb599634260fa358c614
AhmdBlack0 <AhmdBlack.0@gmail.com>
2025-11-19 21:44:13 +0200

    get me

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,6 +1,8 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 // jwt
 // token => { header({alg, HS265}, type(jwt)). payload .alg(secret key)}
@@ -9,7 +11,9 @@ import jwt from "jsonwebtoken";
 export const register = async (req, res) => {
   try {
     const { username, email, password } = req.body;
+
     const hashedPassword = await bcrypt.hash(password, 10);
+
     const newUser = new User({
       username,
       email,
@@ -38,12 +42,27 @@ export const login = async (req, res) => {
     if (!correctPassword) {
       return res.status(400).json({ data: "password is wrong" });
     }
-    const token = jwt.sign(
-      { userId: user._id },
-      "7ecd6956bd56a3e7ddb0dc3f8a1dcb77dca0285465a859c523d63be6c4e16665",
-      { expiresIn: "3h" }
-    );
+    const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
+      expiresIn: "3h",
+    });
+    user.token = token;
+    await user.save();
     return res.status(200).json({ data: user });
+  } catch (error) {
+    res.status(500).json({ error: error.massage });
+  }
+};
+
+// get me
+export const getMe = async (req, res) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decoded = await jwt.verify(
+      token,
+      "7ecd6956bd56a3e7ddb0dc3f8a1dcb77dca0285465a859c523d63be6c4e16665"
+    );
+    const user = await User.findById(decoded.userId);
+    res.status(200).json({ data: user });
   } catch (error) {
     res.status(500).json({ error: error.massage });
   }
--- db/connectDB.js
+++ db/connectDB.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://ahmdblack0_db_user:vTPz3g8DlKuDSU4D@cluster0.bdqztbp.mongodb.net/?appName=Cluster0"
-    );
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB connected successfully");
   } catch (err) {
     console.error("MongoDB connection error:", err);
